09fe161e902e6fc3fb3d8f2a4a42a9e9bc116cf3
Anderson Rodolfo Barbosa <85766831+foxander@users.noreply.github.com>
2024-03-08 08:31:58 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,47 @@
+import os
+
+from setuptools import find_packages, setup
+
+readme = open("README.rst").read()
+
+history = open("CHANGES.rst").read()
+
+install_requires = ["Shapely==1.8.5.post1",
+                    "pandas==1.5.3",
+                    "geopandas==0.12.2",
+                    "pyproject==1.3.1",
+                    "filelock==3.13.1",
+                    "bdc-catalog @ git+https://github.com/brazil-data-cube/bdc-catalog.git@v1.0.1#egg=bdc-catalog"
+                    ]
+
+packages = find_packages()
+
+g = {}
+with open(os.path.join("collection_publisher", "version.py"), "rt") as fp:
+    exec(fp.read(), g)
+    version = g["__version__"]
+
+setup(
+    name="collection-publisher",
+    version=version,
+    packages=packages,
+    zip_safe=False,
+    include_package_data=True,
+    platforms="any",
+    entry_points={
+        'console_scripts': [
+            'collection-publisher = collector_build.cli:cli',
+        ],
+    },
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: GIS",
+   ]
+)
