59daa3fe343f73b7ca65ca99075ecc26ff1af15a
Anderson Rodolfo Barbosa <85766831+foxander@users.noreply.github.com>
2024-04-15 12:59:23 -0300

    Update config.py

--- collection_publisher/config.py
+++ collection_publisher/config.py
@@ -1,34 +1,26 @@
-# Obtém a variável de ambiente "ENVIRONMENT" (pode ser "development" ou "production")
-#environment = os.getenv("ENVIRONMENT")
-
-#if environment == "development":
-    #SQLALCHEMY_DATABASE_URI='postgresql://postgres:1234@my-pg3/bdcdb'
-SQLALCHEMY_DATABASE_URI='postgresql://postgres:secreto@localhost:5432/bdcdb'
-prefixo = '/mnt/c/users/fox/projetos/INPE/biginpe/mnt/dados'
-dir_file_processed = './processed'
-sat_sensor_incluse = ['CBERS','AMAZONIA','GOES',
-                      'WFI','AWFI','MUX']
-logpath = './log'
-prefixo_data = 'data'
-'''else:
-    # Configurações específicas para o ambiente de produção
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
-    prefixo = os.environ.get("COLLECTION_PUBLISHER_PREFIX")
-    dir_file_processed = os.environ.get("COLLECTION_PUBLISHER_CONTAINER_FILE_PROCESSED")
-    sat_sensor_incluse = os.environ.get("COLLECTION_PUBLISHER_LIST").split(',')
-    logpath = os.environ.get("COLLECTION_PUBLISHER_CONTAINER_LOG_DIR")
-    prefixo_data = os.environ.get("COLLECTION_PUBLISHER_PREFIX_DATA")'''
+# Configurações específicas para o ambiente de produção
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+prefixo = os.environ.get("COLLECTION_PUBLISHER_PREFIX")
+dir_file_processed = os.environ.get("COLLECTION_PUBLISHER_CONTAINER_FILE_PROCESSED")
+sat_sensor_incluse = os.environ.get("COLLECTION_PUBLISHER_LIST").split(',')
+logpath = os.environ.get("COLLECTION_PUBLISHER_CONTAINER_LOG_DIR")
+prefixo_data = os.environ.get("COLLECTION_PUBLISHER_PREFIX_DATA")'''
 
 COG_MIME_TYPE = 'image/tiff; application=geotiff; profile=cloud-optimized'
 
-dict_sat = {'CB4A-WFI':'CBERS_4A_WFI',
-            'CB4-WFI':'CBERS_4_AWFI',
-            'CB4-MUX':'CBERS_4_MUX',
-            'AMZ1-WFI':'AMAZONIA_1_WFI',
-            'CBERS-WFI':'CBERS_4+4A_WFI_8D', #CBERS4 + CBERS4a
-            'CBERS4-MUX':'CBERS_4_MUX_2M',
-            'CBERS4-WFI':'CBERS_4_WFI_16D',
-            'GOES-16':'GOES_16_L2_CMI'}
+dict_sat = {'AMZ1-WFI'      :'AMAZONIA_1_WFI',
+            'CB4A-WFI'      :'CBERS_4A_WFI',
+            'CB4-WFI'       :'CBERS_4_AWFI',
+            'CB4-MUX'       :'CBERS_4_MUX',
+            'CBERS-WFI'     :'CBERS_WFI_8D',  #CBERS4 + CBERS4a
+            'CBERS4-MUX'    :'CBERS_4_MUX_2M',
+            'CBERS4-WFI'    :'CBERS_4_WFI_16D',
+            'GOES16-L2'     :'GOES_16_L2_CMI',
+            'GOES13-L3'     :'GOES_13_L3_IMAGER',
+            'landsat'       :'LE07_L2SP',
+            'mod13q1'       :'MODIS_13_XXX',
+            'mod13q1_bundle':'MODIS_13_BUNDLE_XXX',
+            }
 
 assert_list_image = ["CMASK","EVI","NDVI",
                     "CLEAROB","TOTALOB","PROVENANCE",
@@ -39,7 +31,9 @@ assert_list_image = ["CMASK","EVI","NDVI",
                     "st_emis","st_emsd","st_cdist","sr_atmost_opacity","sr_cloud_qa",
                     "red_reflectance","NIR_reflectance","blue_reflectance","MIR_reflectance"
                     "VI_Quality","pixel_reliability","composite_day_of_the_year",
-                    "view_zenith_angle","sun_zenith_angle","relative_azimuth_angle"
+                    "view_zenith_angle","sun_zenith_angle","relative_azimuth_angle",
+                    "visual","B01","B02","B02_1km","B03","B04","B05","B06","B07","B08",
+                    "B09","B10","B11","B12","B13","B14","B15","B16"
                 ]
 
 assert_list_files = ["ang",
@@ -48,4 +42,6 @@ assert_list_files = ["ang",
                     "thumb_large","thumb_small",
                     "bundle"
                 ]
+
+goes_collections = ['GOES16-L2-CMI-1','GOES13-L3-IMAGER-1']  #Coleção e Versão
 
